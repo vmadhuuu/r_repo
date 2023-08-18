bd4d6efde5c02e9e21452b20c171c960f00a0687
Madhumitha <madhuabi01@yahoo.com>
2023-08-18 17:08:58 +0800

    first machine learning model

--- code.R
+++ code.R
@@ -1,2 +1,79 @@
 library(tidyverse)
-library(tidyr)
+install.packages("caret")
+library(caret)
+library(dslabs)
+data(heights)
+
+# define the outcome and predictors
+y <- heights$sex
+x <- heights$height
+set.seed(2007)
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+test_set <- heights[test_index, ]
+train_set <- heights[-test_index, ]
+
+# guess the outcome
+y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% 
+  factor(levels = levels(test_set$sex))
+
+# compute accuracy
+mean(y_hat == test_set$sex)
+
+# compare heights in males and females in our data set
+heights %>% group_by(sex) %>% summarize(mean(height), sd(height))
+
+# now try predicting "male" if the height is within 2 SD of the average male
+y_hat <- ifelse(x > 62, "Male", "Female") %>% factor(levels = levels(test_set$sex))
+mean(y == y_hat)
+
+# examine the accuracy of 10 cutoffs
+cutoff <- seq(61, 70)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  mean(y_hat == train_set$sex)
+})
+data.frame(cutoff, accuracy) %>% 
+  ggplot(aes(cutoff, accuracy)) + 
+  geom_point() + 
+  geom_line() 
+max(accuracy)
+
+best_cutoff <- cutoff[which.max(accuracy)]
+best_cutoff
+
+y_hat <- ifelse(test_set$height > best_cutoff, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+y_hat <- factor(y_hat)
+mean(y_hat == test_set$sex)
+
+# get the metrics
+cm <- confusionMatrix(data = y_hat, reference = test_set$sex)
+
+# access specific metrics
+cm$overall["Accuracy"]
+
+cm$byClass[c("Sensitivity","Specificity", "Prevalence")]
+
+# maximize F-score
+cutoff <- seq(61, 70)
+F_1 <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  F_meas(data = y_hat, reference = factor(train_set$sex))
+})
+
+data.frame(cutoff, F_1) %>% 
+  ggplot(aes(cutoff, F_1)) + 
+  geom_point() + 
+  geom_line()
+
+max(F_1)
+
+best_cutoff_2 <- cutoff[which.max(F_1)]
+best_cutoff_2
+
+y_hat <- ifelse(test_set$height > best_cutoff_2, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+sensitivity(data = y_hat, reference = test_set$sex)
+specificity(data = y_hat, reference = test_set$sex)
